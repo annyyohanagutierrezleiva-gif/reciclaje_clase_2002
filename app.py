1f876a3c8511dd6c38112fbb85e98dc2d78a4f6f
annyyohanagutierrezleiva-gif <annyyohanagutierrezleiva@gmail.com>
2026-06-23 10:27:35 -0600

    Initialize Streamlit app for recycling image classification
    Set up Streamlit app for image classification using MobileNetV2.

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import json
+from pathlib import path
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
 
+st.set_page_config(page_title="Reciclaje IA-ISC". layout= "centered")
+st.title("Modelo Predictivo de Reciclaje Clase de IA-ISC-Campus Comayagua-2026")
+st.write("Suba una imagen para clasificar con el modelo MobileNEtV2 pre_entrenado")
