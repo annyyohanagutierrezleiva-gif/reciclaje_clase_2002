aab9e8a99e5531df71bff96f74462c90d6704248
annyyohanagutierrezleiva-gif <annyyohanagutierrezleiva@gmail.com>
2026-06-23 10:36:12 -0600

    Add model configuration for recycling prediction

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ from PIL import Image
 st.set_page_config(page_title="Reciclaje IA-ISC", layout= "centered")
 st.title("Modelo Predictivo de Reciclaje Clase de IA-ISC-Campus Comayagua-2026")
 st.write("Suba una imagen para clasificar con el modelo MobileNEtV2 pre_entrenado")
+
+IMG_SIZE=(224,224)
+MODEL_DIR=Path("modelo_reciclaje_mobilenet")
+CLASS_PATH= MODEL_DIR/"class_names.json"
+MODEL_PATHS=[MODEL_DIR/"waste_mobilenet.keras",MODEL_DIR/"waste_mobilenet.h5"]
