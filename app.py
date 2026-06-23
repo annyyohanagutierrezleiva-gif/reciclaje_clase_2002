cab83ebdbbc540ca7a13a81d50612aaa5840f66d
annyyohanagutierrezleiva-gif <annyyohanagutierrezleiva@gmail.com>
2026-06-23 10:49:55 -0600

    Update title in app.py to reflect new author

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import tensorflow  as tf
 from PIL import Image
 
 st.set_page_config(page_title="Reciclaje IA-ISC", layout="centered")
-st.title("Modelo predictivo de reciclaje clase de IA-ISC-campus Comayagua-2026-Genesis Medina")
+st.title("Modelo predictivo de reciclaje clase de IA-ISC-campus Comayagua-2026-Anny-Gutierrez")
 st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre-entrenado")
 
 IMG_SIZE= (224,224)
