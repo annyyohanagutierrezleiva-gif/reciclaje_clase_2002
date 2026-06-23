eec0b269601fe8b03dbb5be7c027bcae47f89c66
annyyohanagutierrezleiva-gif <annyyohanagutierrezleiva@gmail.com>
2026-06-23 10:28:51 -0600

    Fix syntax error in set_page_config call

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 
-st.set_page_config(page_title="Reciclaje IA-ISC". layout= "centered")
+st.set_page_config(page_title="Reciclaje IA-ISC", layout= "centered")
 st.title("Modelo Predictivo de Reciclaje Clase de IA-ISC-Campus Comayagua-2026")
 st.write("Suba una imagen para clasificar con el modelo MobileNEtV2 pre_entrenado")
